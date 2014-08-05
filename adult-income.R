6bba37e2cab0110528e27743aa0d328178f21d2d
David Monk <lostintunisia@gmail.com>
2014-08-04 17:54:15 -0700

    minor UI updates

--- adult-income.R
+++ adult-income.R
@@ -4,10 +4,10 @@
 #http://archive.ics.uci.edu/ml/datasets/Adult
 
 require(caret)
+#require(randomForest)
 set.seed(300)
 adult <- read.csv("adult.data", header=FALSE)
-#adult.test <- read.csv("~/R/ddp/adult.test", header=FALSE)
-adult.names <- c("age", "workclass", "fnlwgt", "education", "education-num", "maritalStatus", "occupation", "relationship",
+adult.names <- c("age", "workclass", "fnlwgt", "education", "educationNum", "maritalStatus", "occupation", "relationship",
                  "race", "sex", "capitalGain", "capitalLoss", "hoursPerWeek", "countryOrigin", "income")
 names(adult) <- adult.names
 adult$education <- NULL
@@ -17,10 +17,12 @@ adult$capitalGain <- NULL
 adult$capitalLoss <- NULL
 adult$relationship <- NULL
 adult$workclass <- NULL
-adult.sample <- adult[sample(nrow(adult), 10000), ]
+adult.sample <- adult[sample(nrow(adult), 20000), ]
 inTrain = createDataPartition(adult.sample$income, p = 0.7, list = FALSE)
 training = adult.sample[ inTrain, ]
 validation = adult.sample [ -inTrain, ]
-modFit <- train(income ~ ., data = adult.sample, method = "rf", trControl = trainControl(method='cv'))
+modFit <- train(income ~ ., data = training, method = "gbm", trControl = trainControl(method='cv'))
+#modRf <- randomForest(income ~ ., data = adult.sample) #can't use, countryOrigin has more than 32 factor levels
+#test <- read.csv("adult.test", header=FALSE)
 predict1 <- predict(modFit, validation)
 confusionMatrix(predict1, validation$income)
--- ui.R
+++ ui.R
@@ -8,7 +8,6 @@ maritalStatus <- readRDS("maritalStatus.rds")
 shinyUI(pageWithSidebar(
   headerPanel("USA Standard of Living Predictor"),
   sidebarPanel(
-    helpText("See the predicted standard of living based on selected parameters"),
     selectInput("selectCountry", "Country of Origin", choices = nativeCountry ),
     selectInput("selectOcc", "Occupation", choices = occupation ),
     selectInput("selectRace", "Race", choices = race ),
@@ -16,28 +15,37 @@ shinyUI(pageWithSidebar(
     sliderInput("slideAge", "Age (years)", 18, 80, 30),
     sliderInput("slideSchooling", "Years of formal schooling", 0, 22, 12),
     radioButtons("radioSex", "Choose gender:", list(" Female", " Male")),
-    sliderInput("slideHours", "Hours Per Week of Work", 0, 80, 40),
-    helpText("UCI Census Income: http://archive.ics.uci.edu/ml/datasets/Adult")
+    sliderInput("slideHours", "Hours Per Week of Work", 0, 80, 40)
   ),
   mainPanel(
-    h2('Predicted standard of living'),
-    textOutput('text1'),
-    hr(),
-    h5('Country of Origin'),
-    textOutput('textCountry'),
-    h5('Occupation'),
-    textOutput('textOccupation'),
-    h5('Race'),
-    textOutput('textRace'),
-    h5('Marital Status'),
-    textOutput('textMarital'),
-    h5('Age'),
-    textOutput('textAge'),
-    h5('Years of schooling'),
-    textOutput('textSchooling'),
-    h5('Sex'),
-    textOutput('textSex'),
-    h5('Hours per week'),
-    textOutput('textHours')
+    tabsetPanel(
+      tabPanel("Output",
+        h2('Predicted standard of living'),
+        textOutput('text1'),
+        hr(),
+        h5('Country of Origin'),
+        textOutput('textCountry'),
+        h5('Occupation'),
+        textOutput('textOccupation'),
+        h5('Race'),
+        textOutput('textRace'),
+        h5('Marital Status'),
+        textOutput('textMarital'),
+        h5('Age'),
+        textOutput('textAge'),
+        h5('Years of schooling'),
+        textOutput('textSchooling'),
+        h5('Sex'),
+        textOutput('textSex'),
+        h5('Hours per week'),
+        textOutput('textHours')
+      ),
+      tabPanel("Instructions",
+        helpText("Select the personal characteristics from the sidebar at left and see the predicted lifestyle change in real time"),
+        helpText("The author trained the model from the listed dataset with the personal characteristics and income level"),
+        helpText("Data: UCI Census Income"),
+        helpText("http://archive.ics.uci.edu/ml/datasets/Adult")
+      )
     )
+  )
 ) )
